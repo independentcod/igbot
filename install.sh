2e7047bc9b508442fd69ff55c0198228787113e1
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 00:52:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo 'This repo is made for Kali Chroot in Termux!';
 cp -r /etc/apt/sources.list sources.list.bak;
 echo deb http://ftp.debian.org/debian unstable main contrib non-free >/etc/apt/sources.list;
 apt-get clean;
-apt update && apt install lxde-core tigervnc-standalone-server python python-pip leafpad -y;
+apt update && apt install lxde-core tigervnc-standalone-server python3 libpython3-dev python3-pip leafpad -y;
 cp -r sources.list.bak /etc/apt/sources.list;
 apt-get clean;
 pip3 install -r requirements.txt;

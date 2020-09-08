a9ead187299d2d7e6078f5084fbb04525d59a8a4
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 00:35:51 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-apt update && apt install lxde-core tigervnc-standalone-server python python-pip leafpad -y;
-pip3 install -r requirements.txt;
+echo 'This repo is made for Kali Chroot in Termux!';
+apt update && apt remove python3 python3-pip --purge && apt install lxde-core tigervnc-standalone-server python python-pip leafpad -y;
+pip install -r requirements.txt;
 mkdir ~/.vnc;
 echo lxsession >~/.vnc/xstartup;
 vncserver :1 -localhost no;

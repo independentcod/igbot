e03aeb4d7b7e55b6ed398f32e89e474ffaa9d062
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-09-08 00:27:23 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-apt update && apt install lxde-core tigervnc-standalone-server python3 python3-pip kwrite -y;
+apt update && apt install lxde-core tigervnc-standalone-server python3 python3-pip leafpad -y;
 pip3 install -r requirements.txt;
 mkdir ~/.vnc;
 echo lxsession >~/.vnc/xstartup;
 vncserver :1 -localhost no;
 export DISPLAY=:1;
-kwrite run.py;
+leafpad run.py;
 python3 run.py;
